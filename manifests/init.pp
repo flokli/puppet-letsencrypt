e31fcc1a83562d4c1652d317bd28fb221636b56c
Florian Klink <flokli@flokli.de>
2017-07-27 13:01:37 +0200

    use letsencrypt on xenial, certbot on jessie and stretch

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,37 @@
-class letsencrypt {
-  include letsencrypt::params
+class letsencrypt (
+  String $interval = 'weekly',
+) inherits letsencrypt::params {
+  $package_name = $letsencrypt::params::package_name
 
-  package { 'certbot':
+  package { $package_name:
     ensure => latest,
   }
 
-  file { '/etc/systemd/system/certbot.service':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/letsencrypt/certbot.service',
+  file { '/etc/systemd/system/letsencrypt.service':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('letsencrypt/letsencrypt.service.epp', {
+      'package_name' => $package_name,
+    }),
   }
-  file { '/etc/systemd/system/certbot.service.d':
+  file { '/etc/systemd/system/letsencrypt.service.d':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
   }
-  file { '/etc/systemd/system/certbot.timer':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/letsencrypt/certbot.timer',
+  file { '/etc/systemd/system/letsencrypt.timer':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('letsencrypt/letsencrypt.timer.epp', {
+      'interval' => $interval
+    }),
   }
-  service { 'certbot.timer':
+  service { 'letsencrypt.timer':
     ensure => running,
     enable => true,
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,18 @@ class letsencrypt::params {
     }
     'debian': {
       $webroot = '/var/www/html'
+      $distcodename = $::facts['os']['lsb']['distcodename']
+      case $distcodename {
+        'xenial': {
+          $package_name = 'letsencrypt'
+        }
+        'stretch', 'jessie': {
+          $package_name = 'certbot'
+        }
+        default: {
+          fail("no support for ${package_name}, please add support")
+        }
+      }
     }
     default: {
       fail("no support for {${::osfamily}}. please add support!")
